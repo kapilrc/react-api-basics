65211cbb84b2564707ad8ff4c78316b35f2991ca
kapilrc <kapilrc@gmail.com>
2020-03-28 16:15:24 +0530

    Updates

---
+++ precache-manifest.781bceacf83fde1dccfe2278f5a31edc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5204caa9773b1e052c6b8aec1942e34d",
+    "url": "/monstor-rolodex/index.html"
+  },
+  {
+    "revision": "edffb0ed6a292da80423",
+    "url": "/monstor-rolodex/static/css/main.1e89a97a.chunk.css"
+  },
+  {
+    "revision": "cfceac3283c0ca996338",
+    "url": "/monstor-rolodex/static/js/2.9c424b6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstor-rolodex/static/js/2.9c424b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edffb0ed6a292da80423",
+    "url": "/monstor-rolodex/static/js/main.3b06b15f.chunk.js"
+  },
+  {
+    "revision": "a9332307ef104ff7b3ed",
+    "url": "/monstor-rolodex/static/js/runtime-main.d15e3027.js"
+  }
+]);
